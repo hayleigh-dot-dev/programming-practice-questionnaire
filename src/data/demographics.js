439bc690d9365ec05f350b0db0acceaaec2f6a0d
Andrew Thompson <andrew.thompson@qmul.ac.uk>
2020-01-27 16:25:07 +0000

    :sparkles: Move initial data into separate files.

---
+++ src/data/demographics.js
@@ -0,0 +1,86 @@
+export const singleResponse = (question, options) => ({
+  type: 'SingleResponse',
+  question: question,
+  response: null,
+  options: options.map(text => ({ text, type: 'Fixed' }))
+})
+
+export const singleResponseWithOther = (question, options) => ({
+  type: 'SingleResponseWithOther',
+  question: question,
+  response: null,
+  options: options.map(text => ({ text, type: 'Fixed' }))
+})
+
+export const multipleResponse = (question, options) => ({
+  type: 'MultipleResponse',
+  question: question,
+  response: [],
+  options: options.map(text => ({ text, type: 'Fixed' }))
+})
+
+export const multipleResponseWithOther = (question, options) => ({
+  type: 'MultipleResponseWithOther',
+  question: question,
+  response: [],
+  options: options.map(text => ({ text, type: 'Fixed' }))
+})
+
+export const demographics = [
+  singleResponse('How old are you?', [
+    'Under 18',
+    '18-24 years old',
+    '25-34 yearw old',
+    '45-54 years old',
+    'Over 55'
+  ]),
+  singleResponseWithOther('What is your highest level of education achieved', [
+    'Less than A-levels (UK) or a high school diploma (US)',
+    'A-levels (URK), high school diploma (US), or equivalent',
+    'Bachelor\'s Degree',
+    'Master\'s Degree',
+    'Doctorate'
+  ]),
+  singleResponseWithOther('What was your primary field of study?', [
+    'Computer Science, Software Engineering, or related field of study',
+    'Music, Music Technology, or related field of study'
+  ]),
+  singleResponse('How many years experience do you have programming?', [
+    'Less than 1 year',
+    '1 to 2 years',
+    '2 to 5 years',
+    'Over 5 years'
+  ]),
+  singleResponse('How many years experience do you have writing interactive audio software specifically?', [
+    'Less than 1 year',
+    '1 to 2 years',
+    '2 to 5 years',
+    'Over 5 years'
+  ]),
+  multipleResponseWithOther('Which of the following languages or frameworks have you used for creating interactive audio software?', [
+    'AudioKit [Swift]',
+    'Csound',
+    'FAUST',
+    'JUCE [C++]',
+    'Max/MSP',
+    'Processing',
+    'Pure Data',
+    'Reaktor',
+    'SuperCollider',
+    'Web Audio API [JavaScript]'
+  ]),
+  multipleResponseWithOther('Which of the following languages or frameworks do you have the most experience creating interactive audio software with?', [
+    'AudioKit [Swift]',
+    'Csound',
+    'FAUST',
+    'JUCE [C++]',
+    'Max/MSP',
+    'Processing',
+    'Pure Data',
+    'Reaktor',
+    'SuperCollider',
+    'Web Audio API [JavaScript]'
+  ])
+]
+
+export default demographics
---
+++ src/data/likert.js
@@ -0,0 +1,49 @@
+// Likert scales ---------------------------------------------------------------
+export const likertScale = (title, description, statements) => ({
+  title: title,
+  description: description,
+  statements: statements.map(statement => ({ statement })),
+  ratings: [
+    'Strongly Disagree',
+    'Disagree',
+    'Neutral',
+    'Agree',
+    'Strongly Agree'
+  ]
+})
+
+export const likert = [
+  likertScale(
+    'How well defined is the end goal?',
+    'Some programmers have a very clear idea of what the finished piece of software should look like and what it should do. Others take a more exploratory approach. The following statements are about how clearly defined the end-goal of a project is before you start programming.',
+    [
+      'Before starting to program I have a clear idea what the program will look like.',
+      'Before starting to program I have a clear idea how the program will work.',
+      'Before starting to program I have a clear idea how to organise my code.',
+      'Before starting to program I write down what I need to do.',
+      'Before starting to program I sketch or wireframe how the program will look.'
+    ]
+  ),
+  likertScale(
+    'How much time do you spend on, and how big is, the finished project?',
+    'Some programmers work on projects for many months or even years. Others prefer to work on a project for just a matter of days or hours. The following statements are about how long a typical project takes before you consider it complete, and how large the codebase is when it is finished.',
+    [
+      'A typical project takes a significant amount of time to complete.',
+      'A finished project\'s codebase tends to span multiple files and many lines of code.',
+      'I tend to consider a project finished after one or two sessions of programming.',
+      'A finished project\'s codebase tends to be made up of just one or two files.'
+    ]
+  ),
+  likertScale(
+    'How much did the project change from beginning to end?',
+    '',
+    [
+      'I consider a project finished when all my tests pass.',
+      'I think non-critical bugs can lead to unexpected behaviour that can make a project better or more interesting.',
+      'The primary method of interaction in a program has often changed by the time that project is finished.',
+      'The primary audio output of a program has often changed by the time that project is finished.'
+    ]
+  )
+]
+
+export default likert
